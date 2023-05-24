cba240c07b605a392cd94961250c48bed0050045
Lincoln-Ab <abramodipietro@libero.it>
2023-05-24 21:06:16 +0200

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,8 +11,6 @@ public class PlayerController : MonoBehaviour
 
     public Animator animator;
 
-    public AnimationCurve accelerationCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
-    float inputTime = 0f;
     Vector2 dir = Vector2.zero;
 
     public GameObject bullet;
@@ -20,26 +18,42 @@ public class PlayerController : MonoBehaviour
     GameObject rightWeapon;
 
     bool isRightWeapon = false;
+    Vector2 min;
+    Vector2 max;
+
+    Vector2 residualVelocity;
+
+    float gravityForce = 2f;
+
+    float frictionForce = -3f;
+
+    float accelerationForce = 7f;
 
     // Start is called before the first frame update
     void Start()
     {
-        leftWeapon= transform.Find("LeftWeapon").gameObject;
+        leftWeapon = transform.Find("LeftWeapon").gameObject;
         rightWeapon = transform.Find("RightWeapon").gameObject;
+
+        // cerco limiti dello schermo
+        min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        //aggiungo un extra bordo per evitare che il player esca fuori 
+        min.x += halfSpriteSize.x;
+        min.y += halfSpriteSize.y;
+        max.x -= halfSpriteSize.x;
+        max.y -= halfSpriteSize.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("space"))
-        {
-           InvokeRepeating("Shoot",0f,0.1f);
-        }
+        if (Input.GetKeyDown("space"))
+            InvokeRepeating("Shoot", 0f, 0.1f);
 
-        if(Input.GetKeyUp("space"))
-        {
+        if (Input.GetKeyUp("space"))
             CancelInvoke("Shoot");
-        }
 
         //catturo assi 
         float x = Input.GetAxisRaw("Horizontal");
@@ -48,55 +62,60 @@ public class PlayerController : MonoBehaviour
         // creo vettore direzione input
         Vector2 inputDir = new Vector2(x, y).normalized;
 
-        // creo vettore direzione
 
-        if (inputDir.Equals(Vector2.zero))
+        Move(inputDir);
+    }
+
+    void Move(Vector2 inputDirection)
+    {
+        Vector2 velocity = Vector2.zero;
+
+        if (inputDirection.magnitude > 0)
         {
-            animator.SetBool("IsAccelerating", false);
-            inputTime -= Time.deltaTime;
-            inputTime = Mathf.Clamp(inputTime, 0, 1);
-        }
-        else if (inputDir.y > 0)
-        {
-            animator.SetBool("IsAccelerating", true);
-            dir = inputDir;
-            inputTime += Time.deltaTime;
-            inputTime = Mathf.Clamp(inputTime, 0, 1);
+            velocity = residualVelocity + inputDirection * accelerationForce * Time.deltaTime;
+            Debug.Log(velocity.magnitude);
         }
         else
         {
-            dir = inputDir;
-            inputTime += Time.deltaTime;
-            inputTime = Mathf.Clamp(inputTime, 0, 1);
+            velocity = residualVelocity + residualVelocity * frictionForce * Time.deltaTime;
         }
 
-        Move(dir, inputTime);
-    }
+        velocity += Vector2.down * gravityForce * Time.deltaTime;
 
-    void Move(Vector2 direction, float inputTime)
-    {
-        // cerco limiti dello schermo
-        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
-        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+        residualVelocity = velocity;
 
-        //aggiungo l altezza e la larghezza della sprite per evitare che esca fuori
-        min.x += halfSpriteSize.x;
-        min.y += halfSpriteSize.y;
-        max.x -= halfSpriteSize.x;
-        max.y -= halfSpriteSize.y;
 
         // posizione Player
         Vector2 position = transform.position;
 
-        // calcolo curva di accellerazione
-        float acceleration = accelerationCurve.Evaluate(inputTime);
-
         // posizione aggiornata
-        position += direction * speed * acceleration * Time.deltaTime;
+        position += velocity * Time.deltaTime;
+
+        if (position.x < min.x)
+        {
+            residualVelocity.x = 0f;
+            position.x = min.x;
+        }
+        if (position.x > max.x)
+        {
+            residualVelocity.x = 0f;
+            position.x = max.x;
+        }
+        if (position.y < min.y)
+        {
+            residualVelocity.y = 0f;
+            position.y = min.y;
+        }
+        if (position.y > max.y)
+        {
+            residualVelocity.y = 0f;
+            position.y = max.y;
+        }
+
 
         // costringo il Player nei limiti dello schermo
-        position.x = Mathf.Clamp(position.x, min.x, max.x);
-        position.y = Mathf.Clamp(position.y, min.y, max.y);
+        //position.x = Mathf.Clamp(position.x, min.x, max.x);
+        //position.y = Mathf.Clamp(position.y, min.y, max.y);
 
         // setto nuova posizione del Player
         transform.position = position;
@@ -106,17 +125,17 @@ public class PlayerController : MonoBehaviour
     {
         GameObject bullet1 = (GameObject)Instantiate(bullet);
 
-        if(isRightWeapon)
+        if (isRightWeapon)
         {
             bullet1.transform.position = rightWeapon.transform.position;
-            isRightWeapon=false;
+            isRightWeapon = false;
         }
         else
         {
             bullet1.transform.position = leftWeapon.transform.position;
-            isRightWeapon=true;
+            isRightWeapon = true;
         }
-        
+
 
     }
 }
