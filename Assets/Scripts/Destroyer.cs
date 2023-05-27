d62eca702ff2253d06fbcc08438573b031ee874d
Simone Lungarella <simonelungarella@gmail.com>
2023-05-27 18:41:01 +0200

    feat: created explosion animation that destroy itself after the animation end

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    void DestroyGameObject()
+    {
+        Destroy(gameObject);
+    }
+}
