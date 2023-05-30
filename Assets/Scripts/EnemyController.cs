ca28645db4c9205bd4cd807195635b45e188c2db
Lincoln-Ab <abramodipietro@libero.it>
2023-05-30 19:42:12 +0200

    Update EnemyController.cs

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -17,6 +17,8 @@ public class EnemyController : MonoBehaviour, IHittableInterface
 
     public int explosionDamage = 30;
 
+    bool isDead = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -33,29 +35,33 @@ public class EnemyController : MonoBehaviour, IHittableInterface
     // Update is called once per frame
     void Update()
     {
-        // muovo verso il basso il nemico
-        transform.position += Vector3.down * speed * Time.deltaTime;
-        Vector3 direction;
-        if (player)
+        if (!isDead)
         {
-            direction = player.transform.position - transform.position;
-            direction.Normalize();
+            // muovo verso il basso il nemico
+            transform.position += Vector3.down * speed * Time.deltaTime;
+
+            // recupero la direzione che punta verso il player
+            Vector3 direction;
+            if (player)
+            {
+                direction = player.transform.position - transform.position;
+                direction.Normalize();
+            }
+            else
+            {
+                direction = Vector3.down;
+                CancelInvoke("Shoot");
+            }
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90f;
+
+            // ruoto il nemico nella direzione del player
+            Quaternion targetRotation = Quaternion.Euler(0, 0, angle);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), Time.deltaTime);
+
+            // se il nemico supera il margine imposto viene distrutto
+            if (transform.position.y < screenLimit)
+                Destroy(gameObject);
         }
-        else
-        {
-            direction = Vector3.down;
-            CancelInvoke("Shoot");
-        }
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90f;
-
-        Quaternion targetRotation = Quaternion.Euler(0, 0, angle);
-
-        // Smoothly rotate towards the target rotation
-        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), Time.deltaTime);
-
-        // se il nemico supera il margine imposto viene distrutto
-        if (transform.position.y < screenLimit)
-            Destroy(gameObject);
     }
 
     void Shoot()
@@ -63,35 +69,41 @@ public class EnemyController : MonoBehaviour, IHittableInterface
         // instanzio il proiettile
         GameObject bullet1 = (GameObject)Instantiate(bullet);
 
+        // sparo nella direzione in cui sto puntando
         bullet1.transform.position = gun.transform.position;
         bullet1.transform.rotation = Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z + 180);
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        foreach (var item in hittables)
+        // controllo con chi sono entrato in conflitto e gestisco i casi
+        switch (other.gameObject.tag)
         {
-            if (other.gameObject.CompareTag(item))
-            {
+            default: break;
+            case "PlayerShip":
+                // genero danno alla nave del player sacrificando il nemico
                 IHittableInterface hittableInterface = other.collider.GetComponent<IHittableInterface>();
                 hittableInterface.DealDamage(explosionDamage);
-                GetComponent<Animator>().SetBool("isDead", true);
-            }
+                DealDamage(life);
+                break;
         }
     }
 
     public void DealDamage(int damage)
     {
+        // applico danno alla nave nemica e la distruggo se rimane senza vita
         life -= damage;
         if (life <= 0)
         {
+            isDead = true;
             GetComponent<Animator>().SetBool("isDead", true);
         }
     }
 
-    void DestroyShip()
+    void DestroyShip() // chiamata alla fine dell animazione di morte
     {
-        GameObject.FindGameObjectWithTag("GameController").GetComponent<GameInstance>().UpdateScore(1);
+        // comunico la distruzione al GameInstance per aggiornare il punteggio
+        GameObject.FindGameObjectWithTag("GameController").GetComponent<GameInstance>().UpdateScore(1, true);
         Destroy(gameObject);
     }
 }
